3614a7d6542ffa2de5c2795b3ee500a5c7ac654b
deepika621 <18311a12l2@sreenidhi.edu.in>
2022-09-23 15:09:17 +0530

    commit

---
+++ 17-06-22/currying.js
@@ -0,0 +1,13 @@
+const add=(a,b,c)=>a+b+c;
+console.log(add(4,5,6))
+const curryAdd=sum=>{
+    return (a)=>{
+        return (b)=>{
+            return (c)=>{
+                return sum(a,b,c);
+            }
+        }
+    }
+}
+const result=curryAdd(add);
+console.log(result(6)(2)(1));
