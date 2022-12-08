3b61fe37a2d125a5f7f76b8d8b64b23d48da9eff
deepika621 <18311a12l2@sreenidhi.edu.in>
2022-12-08 21:29:57 +0530

    todo

--- Nov/7-11-22/Todo-app/index.js
+++ Nov/7-11-22/Todo-app/index.js
@@ -1,14 +1,25 @@
+let todos=[];
 function handleToDoFormSubmit(){
     console.log("Form Submitted");
     const txtTodo = document.getElementById("txtTodo");
     console.log(txtTodo);
     const todoTextValue = txtTodo.value;
+    txtTodo.value="";
     console.log(todoTextValue);
     createTodoListItem(todoTextValue);
 }
 function createTodoListItem(text){
-    const li="<li><label>"+text+"</label><button>Edit</button><button>Delete</button></li>"
+    todos.push(text)
+    const li="<li><label id=todo"+todos.length+" style='width:80%'>"+text+"</label><button id=editBtn"+todos.length+" onClick='handleEditClick(event)' class='btn btn-primary'>Edit</button>&nbsp;&nbsp;<button class='btn btn-danger'>Delete</button></li>"
     const ul=document.getElementById("todoList");
     //append li to inner html of ul//
     ul.innerHTML=ul.innerHTML+li;
+}
+function handleEditClick(event){
+    console.log(event.target.id);
+    const labelID=event.target.id.toString().replace("editBtn","todo");
+    const label=document.getElementById(labelID);
+    label.style.display="none";
+    console.log(label);
+    console.log("handling edit click");
 }
