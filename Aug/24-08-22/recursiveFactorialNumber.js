2d32e84b18c7c84dbf30119ddd32babd251af5b0
Deepika <109918182+deepika621@users.noreply.github.com>
2023-05-17 10:03:39 +0530

    Recursion

--- Aug/24-08-22/recursiveFactorialNumber.js
+++ Aug/24-08-22/recursiveFactorialNumber.js
@@ -4,9 +4,11 @@ function findFactorial(num){
     //if(num==0){
         //return 1;
     //}
+    //Base Condition
     if(num<=1){
         return 1;
     }
     return num*findFactorial(num-1)
 }
+//Start of Recursion
 console.log(findFactorial(5))
