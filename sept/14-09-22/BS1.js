e53c3f07cc850fb267528162a6c604d7ccf5751b
Deepika <109918182+deepika621@users.noreply.github.com>
2023-06-05 10:47:05 +0530

    Practice

---
+++ sept/14-09-22/BS1.js
@@ -0,0 +1,22 @@
+//let n=arr.length;
+function BubbleSort(arr,k){
+    for(var i=0;i<k;i++){
+        var isSwapped=false
+        for(var j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                var temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                isSwapped=true
+            }
+        }
+        if(!isSwapped)
+        break
+    }
+    return arr;
+    //console.log(arr);
+}
+var k=4;
+var arr=[5,4,3,2,1,6,2]
+arr=BubbleSort(arr,k)
+console.log(arr[arr.length-4])
